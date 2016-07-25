7a15035c9fd4ff198c449e8103b29096c9a018ce
rpadalka <ramanpadalka@gmail.com>
2016-07-25 16:44:41 +0300

    Add custom scope

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -13,7 +13,7 @@ import java.util.Random;
 public class Config {
 
     @Bean
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
